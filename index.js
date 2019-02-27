ca63b0b9ac55f456439fa54a4ae5588527b1b81f
Jim Schemmel <james73047@gmail.com>
2019-02-27 11:40:42 -0600

    Update directory with /assets, /routes, /templates, /templates/homes, and /templates/residents.  Change index.js accordingly.

--- index.js
+++ index.js
@@ -4,41 +4,37 @@ var app = express();
 
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/images')));
-
-app.get('/api/home-data', function(request, response) {  // Uncertain, is home-data name correct?
-  response.send(JSON.parse({ key: "value"}));
-})
+app.use(express.static(path.join(__dirname, '/assets/images')));
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/" + "home-html/homepage.html");
+  response.sendFile(__dirname + "/" + "templates/homes/homepage.html");
 });
 
 app.get("/homes", function(request, response) {
-  response.sendFile(__dirname + "/" + "home-html/index.html");
+  response.sendFile(__dirname + "/" + "templates/homes/index.html");
 });
 
 app.get("/homesedit", function(request, response) {
-  response.sendFile(__dirname + "/" + "home-html/edit.html");
+  response.sendFile(__dirname + "/" + "templates/homes/edit.html");
 });
 
 app.get("/homescreate", function(request, response) {
-  response.sendFile(__dirname + "/" + "home-html/create.html");
+  response.sendFile(__dirname + "/" + "templates/homes/create.html");
 });
 
 app.get('/residents', function(request, response) {
-  response.sendFile(__dirname + "/" + "resident-html/index.html");
+  response.sendFile(__dirname + "/" + "templates/residents/index.html");
 });
 
 app.get('/residentsedit', function(request, response) {
-  response.sendFile(__dirname + "/" + "resident-html/edit.html");
+  response.sendFile(__dirname + "/" + "templates/residents/edit.html");
 });
 
 app.get('/residentscreate', function(request, response) {
-  response.sendFile(__dirname + "/" + "resident-html/create.html");
+  response.sendFile(__dirname + "/" + "templates/residents/create.html");
 });
 app.get('/residentsread', function(request, response) {
-  response.sendFile(__dirname + "/" + "resident-html/read.html");
+  response.sendFile(__dirname + "/" + "templates/residents/read.html");
 });
 
 
@@ -46,4 +42,4 @@ app.listen(5000, function() {
   console.log('server is running');
 });
 
-module.exports = app;  //Is "app" correct name?  Confusing!
+module.exports = app;
