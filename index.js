ff5385c342573d9a4ec3af17ae08f8b4f721c826
Jim Schemmel <james73047@gmail.com>
2019-03-21 14:54:41 -0500

    Fix directory reference to /routes

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 //App for Lake Quivira Community Directory
 var express = require('express');
-var routes = require('./router');
+var routes = require('./routes/router');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/assets/images')));
 var mongoose = require('./mongoose_basics/node_modules/mongoose');
-mongoose.connect('mongodb://localhost:5000/lqdb1', {useNewUrlParser: true}, function (error) {
-  if (error) throw error;
-  console.log('Successfully connected to mongoose database');
-});
+//mongoose.connect('mongodb://localhost:5000/lqdb1', {useNewUrlParser: true}, function(err) {
+//  if (err) throw err;
+//  console.log('Successfully connected to mongoose database');
+//});
+
+mongoose.Promise = require('./mongoose_basics/node_modules/bluebird');
+mongoose.connect('mongodb://localhost:5000/lqdb1', { useNewUrlParser: true, promiseLibrary: require('./mongoose_basics/node_modules/bluebird') })
+  .then(() =>  console.log('mongoose connection succesful'))
+  .catch((err) => console.error(err));
 
 //Routes
-app.use(require('./router'));  //http://localhost:5000 (...)
+app.use(require('./routes/router'));  //http://localhost:5000 (...)
 
 var server = app.listen(5000, function () {
 
--- routes/router.js
+++ routes/router.js
@@ -3,40 +3,40 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 app.use(router);
-
+var path = require('path');
 //Middle ware that is specific to this router would go here.
 
 //Define the "Home Page" route.
 router.get("/", function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/homes/homepage.html");
+  response.sendFile(path.join(__dirname, "../templates/homes", "homepage.html"));
 });
 //Define the "Home Directory" route.
 router.get("/homes", function(request, response) {
-response.sendFile(__dirname + "/" + "templates/homes/index.html");
+response.sendFile(path.join(__dirname, "../templates/homes", "index.html"));
 });
 //Define the "Home Edit" route.
 router.get("/homesedit", function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/homes/edit.html");
+  response.sendFile(path.join(__dirname, "../templates/homes", "edit.html"));
 });
 //Define the "Home Create" route.
 router.get("/homescreate", function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/homes/create.html");
+  response.sendFile(path.join(__dirname, "../templates/homes", "create.html"));
 });
 //Define the "Residents Directory" route.
 router.get('/residents', function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/residents/index.html");
+  response.sendFile(path.join(__dirname, "../templates/residents", "index.html"));
 });
 //Define the "Resident Edit" route.
 router.get('/residentsedit', function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/residents/edit.html");
+  response.sendFile(path.join(__dirname, "../templates/residents", "edit.html"));
 });
 //Define the "Resident Create" route.
 router.get('/residentscreate', function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/residents/create.html");
+  response.sendFile(path.join(__dirname, "../templates/residents", "create.html"));
 });
 //Define the "Resident Read" route.
 router.get('/residentsread', function(request, response) {
-  response.sendFile(__dirname + "/" + "templates/residents/read.html");
+  response.sendFile(path.join(__dirname, "../templates/residents", "read.html"));
 });
 
 module.exports = router;
