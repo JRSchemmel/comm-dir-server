3d52c185e6ea349b864c825cd4919a7807e8a810
Jim Schemmel <james73047@gmail.com>
2019-04-04 14:35:22 -0500

    3rd updates for mongodb, still not working

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ var routes = require('./routes/routes');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/assets/images')));
+
+app.listen(5000, () => {
+  console.log('Running on port 5000');
+});
+
+//var server = app.listen(5000, function () {
+//  var host = server.address().address
+//  var port = server.address().port
+
+//console.log("App listening at http://%s:%s", host, port)
+
 //mongoose.connect('mongodb://localhost:5000/lqdb1', {useNewUrlParser: true}, function(err) {
 //  if (err) throw err;
 //  console.log('Successfully connected to mongoose database');
 //});
 mongoose.Promise = global.Promise;
 mongoose.Promise = require('./mongoose_basics/node_modules/bluebird');
-mongoose.connect('mongodb://localhost:5000/lqdb1', { useNewUrlParser: true, promiseLibrary: require('./mongoose_basics/node_modules/bluebird') })
+mongoose.connect('mongodb://localhost:5000/lqdb1', { useNewUrlParser: true,
+  promiseLibrary: require('./mongoose_basics/node_modules/bluebird') })
   .then(() =>  console.log('mongoose connection succesful'))
   .catch((err) => console.error(err));
 
@@ -23,11 +35,6 @@ routes(app);
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
-
-//var server = app.listen(5000, function () {
-
-//  var host = server.address().address
-//  var port = server.address().port
 
   console.log("App listening at http://%s:%s", host, port)
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 var HomeController = require('../templates/Models/controllers/home_controller');
 
 module.exports = (app) => {
-  app.post('/api/home', HomeController.create);
-  app.put('/api/home/:id', HomeController.edit);
-  app.delete('/api/home/:id', HomeController.delete);
-  app.get('/api/home', HomeController.index);
+  app.post('/api/home', function(req, res){HomeController.create});
+  app.put('/api/home/:id', function(req, res){HomeController.edit});
+  app.delete('/api/home/:id', function(req, res){HomeController.delete});
+  app.get('/api/home', function(req, res){HomeController.index});
 };
