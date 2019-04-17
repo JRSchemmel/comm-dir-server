83676fb3235f6589e28e72b407db227551ed6ab6
Jim Schemmel <james73047@gmail.com>
2019-04-17 10:00:36 -0500

    Deleted extraneous debug code, access to web server and Mongodb works.

--- index.js
+++ index.js
@@ -4,80 +4,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const router = require('./routes/router');
-//const path = require('path');
-//console.log ('path from index file:', require('path'));
-//app.use(express.static(path.join(__dirname, '/assets/images')));
-
-//app.listen(5000, () => {
-//  console.log('Running on port 5000');
-//});
-
 const connect = require('connect');
 const serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(5000, function(){
-    console.log('Server running on 5000...');
-    console.log ('__dirname: ', __dirname);
-});
-
-//const server = app.listen(5000, function () {
-//  const host = server.address().address;
-//  const port = server.address().port;
-//  console.log('App listening at http://%s:%s', host, port)});
-
-//mongoose.Promise = require('./mongoose_basics/node_modules/bluebird');
+connect().use(serveStatic(__dirname)).listen(27017, function(){
+    console.log('Server running on 27017 for MongoDB...');});
 const mongoose = require('./mongoose_basics/node_modules/mongoose');
-// mongoose.Promise = global.Promise;
-
-//mongoose.Promise = require('./mongoose_basics/node_modules/bluebird')
-//  .then(() =>  console.log('mongoose promise succesful'))
-//  .catch((err) => console.error(err));
-console.log('I got this far in index.js');
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:5000/lqdb1', {useNewUrlParser: true}, function(err) {
-//   if (err) throw err
-//   console.log('mongoose.connect:', 'Successfully connected to mongoose database')}
-// );
-
-const dbPath = 'mongodb://localhost:27017/lgdb1';
 mongoose.Promise = global.Promise;
+mongoose.Promise = require('./mongoose_basics/node_modules/bluebird');
+const dbPath = 'mongodb://localhost:27017/lgdb1';
 mongoose.connect(dbPath, { useNewUrlParser: true })
   .then(response => console.log('Mongoose connected'))
   .catch(err => console.error('mongoose.connect', err));
-
-//mongoose.connect('mongodb://localhost:5000/data/lqdb1', { useNewUrlParser: true,
-//  promiseLibrary: require('./mongoose_basics/node_modules/bluebird/js/release/bluebird') })
-//  .then(() =>  console.log('mongoose connection succesful'))
-//  .catch((err) => console.error(err));
-
-//var MongoClient = require('./mongoose_basics/node_modules/mongodb').MongoClient;
-//var assert = require('assert');
-//var url = 'mongodb://localhost:5000';
-//var dbName = 'lqdb1'; { useNewUrlParser: true };
-//var client = new MongoClient(url);
-//client.connect(function(err) {
-//  assert.equal(null, err);
-//  console.log("Connected successfully to server");
-
-//  var db = client.db(dbName);
-
-//  client.close();
-//});
-
-//  app.listen(5000, () => {
-//    console.log('Running on port 5000');
-//  });
-
-//Routes
-//app.use(require('./routes/routes'));  //http://localhost:5000 (...)
-//app.use(bodyParser.json());
-//routes(app);
-
-//app.use((err, req, res, next) => {
-//  res.status(422).send({ error: err.message });
-//
-// console.log("App listening at http://%s:%s", host, port)
-//
-//});
 
 module.exports = app;
--- routes/router.js
+++ routes/router.js
@@ -1,77 +1,26 @@
 //This file is used to place "routes" in its own file.
 const express = require('express');
-//const router = express();
 const app = express();
 const router = express.Router();
-//const router = require('/router');
 const path = require('path');
-console.log("path.join()router : ", path.join(__dirname, "/router"));
-//router.use(router);
 app.use(router);
-//console.log ('got to router module');
-//router.use(express.static(path.join(__dirname, '../assets/images')));
 app.use(express.static(path.join(__dirname, '../assets/images')));
-//console.log('Url: ', req.originalUrl);
-//app.use(function(req, res, next) {
-//  console.log('Url: ', req.originalUrl);
-//  res.send(req.originalUrl);
-//    });
-//console.log ('path:', require('path'));
 const bodyParser = require('body-parser');
 //Middle ware that is specific to this router would go here.
 
-//Define the "Home Page" route.
-
-//router.get("/", (res, req) => res.end(
-//app.use(function(req, res, next) {
-//  console.log('Url: ', req.originalUrl)));
-//  res.send(req.originalUrl);
-
+//Define the "Homes Page" route.
 router.get("/", function (request, response) {
   response.sendFile(path.join(__dirname, "../templates/homes", "homepage.html"));
 });
-//Define the "Home Directory" route.
-const p = '...';
-router.get("/homes", function(request, response, next) {
-  p.then (successful => {
-    if (successful) {
-      console.log ('got to router.get /homes module');
-      response.sendFile(path.join(__dirname, "../templates/homes", "index.html"));
-    }
-    else {
-      console.log ('/homes:');
-    }
-  });
+//Define the "Homes Directory" route.
+router.get("/homes", function(request, response) {
+  response.sendFile(path.join(__dirname, "../templates/homes", "index.html"));
 });
-
-//router.get("/homes", function(request, response, next) {
-//  console.log ('got to router.get /homes module')
-//response.sendFile(path.join(__dirname, "../templates/homes", "index.html"));
-//  next(console.log ('/homes:'));
-//});
-
-//router.get("/homes", function(request, response) {
-//  console.log ('got to router.get /homes module')
-//response.sendFile(path.join(__dirname, "../templates/homes", "index.html"));
-//});
-
-//console.log ('req.params: ', router.get(req.params));
-router.get('/', (request, response) => {  //Test if server is actually working.
-  console.log(`URL: ${request.url}`);  //Display URL on console
-  response.send('Hello, Server!');     //Display "Hello, Server!" on web response
-});
-//console.log('URL: ', ${request.url});
-//console.log ("URL: ", Request.RawUrl);
-//var util = require('util'); // core module
-
-//console.log ("arguments contents are: ", (arguments));
-//console.log ('got to router.get module');
-
-//Define the "Home Edit" route.
+//Define the "Homes Edit" route.
 router.get("/homesedit", function(request, response) {
   response.sendFile(path.join(__dirname, "../templates/homes", "edit.html"));
 });
-//Define the "Home Create" route.
+//Define the "Homes Create" route.
 router.get("/homescreate", function(request, response) {
   response.sendFile(path.join(__dirname, "../templates/homes", "create.html"));
 });
@@ -92,16 +41,8 @@ router.get('/residentsread', function(request, response) {
   response.sendFile(path.join(__dirname, "../templates/residents", "read.html"));
 });
 
-//var util = require('util'); // core module
-//console.log('util: ', util.inspect(request));
-
-//app.use(function(req, res, next) {
-//  console.log('Url: ', req.originalUrl);
-//  res.send(req.originalUrl);
-    //});
-
-console.log("path.join()create.html : ", path.join(__dirname, "../templates/homes", "create.html"));
-
-console.log('router.get: ', router.get);
+app.listen(5000, () => {
+  console.log('Server running on port 5000 for front-end web server');
+  });
 
 module.exports = router;
